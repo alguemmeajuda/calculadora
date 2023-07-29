5c09b9adb40379a578a750c96466f3b313869378
alguemmeajuda <124810553+alguemmeajuda@users.noreply.github.com>
2023-07-29 12:34:43 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,27 @@
 function somar() {
-  var v1 = document.getElementById("n1");
-  var v2 = document.getElementById("n2");
-  var v3 = v1 + v2;
+  var v1 = document.getElementById("n1").value;
+  var v2 = document.getElementById("n2").value;
+  var v3 = parseInt("n1").value + parseInt("n2").value;
+  alert(v3).value;
+}
+
+function subtrair() {
+  var v1 = document.getElementById("n1").value;
+  var v2 = document.getElementById("n2").value;
+  var v3 = v1 - v2;
+  alert(v3);
+}
+
+function multiplicar() {
+  var v1 = document.getElementById("n1").value;
+  var v2 = document.getElementById("n2").value;
+  var v3 = v1 * v2;
+  alert(v3);
+}
+
+function dividir() {
+  var v1 = document.getElementById("n1").value;
+  var v2 = document.getElementById("n2").value;
+  var v3 = v1 / v2;
   alert(v3);
 }
