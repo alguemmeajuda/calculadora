153377df197a9c05b2e5fbc4d129313ad0cfe629
alguemmeajuda <124810553+alguemmeajuda@users.noreply.github.com>
2023-08-02 15:23:06 -0300

    Atualizar o calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,8 @@
 function somar() {
   var v1 = document.getElementById("n1").value;
   var v2 = document.getElementById("n2").value;
-  var v3 = parseInt("n1").value + parseInt("n2").value;
-  alert(v3).value;
+  var v3 = parseInt(v1) + parseInt(v2);
+  alert(v3);
 }
 
 function subtrair() {
